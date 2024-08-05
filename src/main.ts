b5bfa1c9a22aeb9f7b1834566e12ac9ee79cc224
JohnsonKamanga <JohnsonKamanga2@gmail.com>
2024-08-05 14:02:13 +0200

    updated port fetching logic

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");//all endpoints are accessiblbe through api eg. api/recipe
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT',3000);
   app.enableCors();
-  await app.listen(8000);
+  await app.listen(port);
 }
 bootstrap();
